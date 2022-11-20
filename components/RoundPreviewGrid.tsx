7df2d9197544e066555fd89474e8ffecc187cd93
alex olivares <alexander.b.olivares@gmail.com>
2022-11-19 22:28:14 -0600

    Round prview data grid is up and running

--- components/RoundPreviewGrid.tsx
+++ components/RoundPreviewGrid.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import { IRoundPreview } from "../pages/[username]/profile";
+import { useRouter } from "next/router";
 
 interface IRoundPreviewProps {
   roundPreview: IRoundPreview[];
@@ -11,20 +12,20 @@ const columns: GridColDef[] = [
   {
     field: "Date",
     headerName: "Date",
-    width: 90,
+    width: 180,
     valueGetter: (params: GridValueGetterParams) => `${params.row.round_date || ""}`,
   },
   {
     field: "Course",
     headerName: "Course",
-    width: 150,
+    width: 200,
     editable: true,
     valueGetter: (params: GridValueGetterParams) => `${params.row.course_name || ""}`,
   },
   {
     field: "Score",
     headerName: "Score",
-    width: 150,
+    width: 30,
     editable: true,
     valueGetter: (params: GridValueGetterParams) => `${params.row.score || ""}`,
   },
@@ -62,17 +63,41 @@ const columns: GridColDef[] = [
 ];
 
 const RoundPreviewGrid: React.FC<IRoundPreviewProps> = ({ roundPreview }: IRoundPreviewProps) => {
+  const router = useRouter();
+  const username = router.query.username;
+
+  function viewRound(row: IRoundPreview) {
+    const {
+      course_id: courseId,
+      round_id: roundid,
+      tee_color: teeColor,
+      is_user_added_course: isUserAddedCourse,
+    } = row;
+    router.push(
+      {
+        pathname: `/${username}/round/${roundid}`,
+        query: {
+          roundid,
+          courseId,
+          teeColor,
+          isUserAddedCourse,
+        },
+      },
+      `/${username}/round/${roundid}`
+    );
+  }
+
   return (
-    <Box sx={{ height: 400, width: "100%" }}>
+    <Box sx={{ height: 700, width: "100%" }}>
       <DataGrid
         getRowId={row => row.round_id}
         rows={roundPreview}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        // checkboxSelection
-        // disableSelectionOnClick
-        // experimentalFeatures={{ newEditingApi: true }}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
+        onRowClick={row => viewRound(row.row)}
+        density="standard"
+        sortModel={[{ field: "Date", sort: "desc" }]}
       />
     </Box>
   );
