29187a4d57b8555eddebe6db8f1120dc444cef44
alex olivares <alexander.b.olivares@gmail.com>
2023-03-10 22:34:22 -0600

    migrate dev pg to railway. Dev container config

--- db/dbConfig.ts
+++ db/dbConfig.ts
@@ -1,24 +1,12 @@
 import { Pool } from "pg";
 
-const devConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+const pgConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
 
-
-const prodConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
-
-const pool: Pool = new Pool(
-   process.env.NODE_ENV === "production" ? prodConfig : devConfig
-);
-
+const pool: Pool = new Pool(pgConfig);
 
 export default pool;
